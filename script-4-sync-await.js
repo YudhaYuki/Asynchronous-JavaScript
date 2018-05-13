b2381015e477943ab0d2b2c8a28427e8727cf9fb
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-13 13:16:15 +0200

    Making AJAX calls with Fetch and Promises - Ex. 1 - AJAX call using Fetch and Promises

---
+++ script-4-sync-await.js
@@ -0,0 +1,66 @@
+// Promises to Async/Await
+
+
+const getIDs = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve([523, 883, 432, 974]);
+    }, 1500);
+});
+
+const getRecipe = recID => {
+    return new Promise((resolve, reject) => {
+        setTimeout(ID => {
+            const recipe = {
+                title: 'Fresh tomato pasta',
+                publisher: 'Yudha'
+            };
+            resolve(`${ID}: ${recipe.title}`);
+        }, 1500, recID);
+    });
+};
+
+const getRelated = publisher => {
+    return new Promise((resolve, reject) => {
+        setTimeout(pub => {
+            const recipe = {
+                title: 'Italian Pizza',
+                publisher: 'Yudha'
+            };
+            resolve(`${pub} : ${recipe.title}`);
+        }, 1500, publisher);
+    }); 
+};
+
+/*
+getIDs
+.then(IDs => {
+    console.log(IDs);
+    return getRecipe(IDs[2]);
+})
+.then(recipe => {
+    console.log(recipe);
+    return getRelated('Yudha Yuki');
+})
+.then(recipe => {
+    console.log(recipe);
+})
+.catch(error => {
+    console.log('Error !!!');
+});
+*/
+
+
+async function getRecipesAW() {
+    const IDs = await getIDs;
+    console.log(IDs);
+
+    const recipe = await getRecipe(IDs[2]);
+    console.log(recipe);
+
+    const related = await getRelated('Yudha Yuki');
+    console.log(related);
+
+    return recipe;
+}
+
+getRecipesAW().then(result => console.log(`${result} is the best ever !`));
