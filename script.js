fa8d85711eca8e1542ba34fae845d79f7f577ef4
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-13 11:34:33 +0200

    From Callback Hell to Promises - Ex 2

--- script.js
+++ script.js
@@ -6,11 +6,42 @@ const getIDs = new Promise((resolve, reject) => {
     }, 1500);
 });
 
+const getRecipe = recID => {
+    return new Promise((resolve, reject) => {
+        setTimeout(ID => {
+            const recipe = {
+                title: 'Fresh tomato pasta',
+                publisher: 'Yudha'
+            };
+            resolve(`${ID}: ${recipe.title}`);
+        }, 1500, recID);
+    });
+};
+
+const getRelated = publisher => {
+    return new Promise((resolve, reject) => {
+        setTimeout(pub => {
+            const recipe = {
+                title: 'Italian Pizza',
+                publisher: 'Yudha'
+            };
+            resolve(`${pub} : ${recipe.title}`);
+        }, 1500, publisher);
+    }); 
+};
+
 getIDs
 .then(IDs => {
     console.log(IDs);
+    return getRecipe(IDs[2]);
 })
-
+.then(recipe => {
+    console.log(recipe);
+    return getRelated('Yudha Yuki');
+})
+.then(recipe => {
+    console.log(recipe);
+})
 .catch(error => {
     console.log('Error !!!');
 });
