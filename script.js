2d808fe3d025bd95b03bca35c09123c6342d9bbd
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-13 13:47:28 +0200

    Making AJAX calls with Fetch and Promises - Ex. 2 - AJAX call using Fetch and Promises - for London and SF today's and tomorrow's weather

--- script.js
+++ script.js
@@ -3,16 +3,35 @@
 function getWeather(woeid) {
     fetch(`http://crossorigin.me/https://www.metaweather.com/api/location/${woeid}/`)
     .then(result => {
-        console.log(result);
+        // console.log(result);
         return result.json();
     })
     .then(data => {
         // console.log(data);
         const today = data.consolidated_weather[0];
-        console.log(`Temperatures in ${data.title} stay between ${today.min_temp} and ${today.max_temp}.`);
+        console.log(`Temperatures today in ${data.title} stay between ${today.min_temp} and ${today.max_temp}.`);
     })
     .catch(error => console.log(error));
 }
 
 getWeather(2487956);
 getWeather(44418);
+
+
+
+//Another simpler way
+async function getWeatherAW(woeid) {
+    try {
+        const result = await fetch(`http://crossorigin.me/https://www.metaweather.com/api/location/${woeid}/`);
+        const data = await result.json();
+        const tomorrow = data.consolidated_weather[1];
+        console.log(`Temperatures tomorrow in ${data.title} stay between ${tomorrow.min_temp} and ${tomorrow.max_temp}.`);
+    } catch(error) {
+        console.log(error);
+    }
+
+}
+
+getWeatherAW(2487956);
+getWeatherAW(44418);
+
